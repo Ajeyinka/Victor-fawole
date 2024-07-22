19aa79682b4ff5d57eb4fa7ae932e2720dc25674
ajeyinka <ajeyinka12@gmail.com>
2024-07-22 20:31:41 +0100

    HH

--- script.js
+++ script.js
@@ -1,4 +1,148 @@
 
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('darkMode'); 
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('darkmode');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('darkmode');
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('darkMode', null);
+}
+ 
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem('darkMode'); 
+  
+  // if it not current enabled, enable it
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  // if it has been enabled, turn it off  
+  } else {  
+    disableDarkMode(); 
+  }
+});
+
+  
+
+
+
+
+const magnetoElements = document.querySelectorAll('.name');
+
+const activateMagneto = (event) => {
+    const magneto = event.currentTarget;
+    const magnetoText = magneto.querySelector('h3');
+    let boundBox = magneto.getBoundingClientRect();
+    let magnetoStrength = 20;
+    let magnetoTextStrength = 40;
+    const newX = ((event.clientX - boundBox.left) / magneto.offsetWidth) - 0.5;
+    const newY = ((event.clientY - boundBox.top) / magneto.offsetHeight) - 0.5;
+
+    gsap.to(magneto, {
+        duration: 1,
+        x: newX * magnetoStrength,
+        y: newY * magnetoStrength,
+        ease: Power4.easeOut
+    });
+
+    gsap.to(magnetoText, {
+        duration: 1,
+        x: newX * magnetoTextStrength,
+        y: newY * magnetoTextStrength,
+        ease: Power4.easeOut
+    });
+}
+
+const resetMagneto = (event) => {
+    const magneto = event.currentTarget;
+    const magnetoText = magneto.querySelector('h3');
+
+    gsap.to(magneto, {
+        duration: 1,
+        x: 0,
+        y: 0,
+        ease: Elastic.easeOut
+    });
+
+    gsap.to(magnetoText, {
+        duration: 1,
+        x: 0,
+        y: 0,
+        ease: Elastic.easeOut
+    });
+}
+
+magnetoElements.forEach((magneto) => {
+    magneto.addEventListener('mousemove', activateMagneto);
+    magneto.addEventListener('mouseleave', resetMagneto);
+});
+
+
+
+/* const secs = document.querySelectorAll('.section')
+
+
+secs.forEach((sec,index) =>{
+
+    let nans = sec.querySelectorAll('.nan')
+
+    let tL = gsap.timeline({
+        scrollTrigger:{
+            trigger:sec,
+            start:"top 40%",
+            end: "bottom 100%",
+            markers:false,
+            scrub:true,
+            toggleAction:"play pause reverse reset"
+        
+        }
+    });
+    
+    tL.from(nans ,{
+        opacity:0,
+        stagger:{each:0.2},
+        y:"6rem",
+    
+        duration:1,
+    }) 
+    
+})
+
+
+
+
+
+
+let loadTl = gsap.timeline();
+loadTl.from('.txt',{
+    opacity:0,
+    stagger:{each:0.2},
+    y:"6rem",
+    duration:1,
+}
+)
+loadTl.from('.heroes',{
+    opacity:0,
+    scale:0.8,
+    duration:1
+})
+ */
 
 
     let valueDisplays = document.querySelectorAll (".num");
